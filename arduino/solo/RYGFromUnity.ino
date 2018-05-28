c5fe7d08a91949af60b47ec53dadd431c99d1854
rsees5 <38869511+rsees5@users.noreply.github.com>
2018-05-28 17:33:24 +1200

    Add files via upload

---
+++ arduino/solo/RYGFromUnity.ino
@@ -0,0 +1,64 @@
+#include <String.h>
+
+
+int gLed = 10;
+int yLed = 11;
+int rLed = 12;
+char myCol[20];
+bool gOn = true;
+bool yOn = true;
+bool rOn = true;
+
+void setup() {  
+   Serial.begin (9600);  
+   pinMode(gLed, OUTPUT);    
+   pinMode(yLed, OUTPUT);   
+   pinMode(rLed, OUTPUT);   
+  
+   digitalWrite(gLed, HIGH);
+   digitalWrite(yLed, HIGH);
+   digitalWrite(rLed, HIGH); 
+ 
+}
+
+
+void loop() {
+  int lf = 10;
+  Serial.readBytesUntil(lf, myCol, 1);
+    if(strcmp(myCol,"r")==0){             //if myCol string is "r"
+      if(rOn == false){                   //if the LED is off
+        digitalWrite(rLed, HIGH);         //turn the LED on
+        rOn = true;                       //set marker that LED is on
+        //Serial.print("r");                //print to serial - for debugging
+      }
+      else { //if the LED is on
+        digitalWrite(rLed, LOW); //turn LED off
+        rOn = false; //set marker that LED is off       
+      }
+   }
+  if(strcmp(myCol,"y")==0){
+    if(yOn == false) {
+      digitalWrite(yLed, HIGH);
+      yOn = true;
+      //Serial.print("y");
+    }
+    else {
+      digitalWrite(yLed, LOW);
+      yOn = false;
+    }
+   }
+  if(strcmp(myCol,"g")==0){
+    if(gOn == false) {
+      digitalWrite(gLed, HIGH);
+      gOn = true;
+      //Serial.print("g");
+    }
+    else {
+      digitalWrite(gLed, LOW);
+      gOn = false;
+    }
+   }
+   memset(myCol, 0, sizeof myCol); //clears serial log
+
+}
+
